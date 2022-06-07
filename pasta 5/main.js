919465b01fe689f191f66360180c519acc1e224e
Davi Ramalho <daviramalho3011@gmail.com>
2022-06-06 21:52:57 -0300

    nada

---
+++ pasta 5/main.js
@@ -0,0 +1,33 @@
+        
+//----- Variaveis -----// 
+    var name = "Davi";
+    var age = 6;
+    var weight = 62.3;
+    var waked = true;
+
+//-----   ALERT ------//
+//alert("Olá " + name + "!");
+
+// if (waked == true){
+//    alert("Davi esta acordado");
+//} else {
+//    alert("Davi esta dormindo");
+//}
+
+//waked == true ? console.log("Davi esta acordado") : console.log("Davi esta dormindo");
+//var dia = prompt("Que dia é hoje?!");
+//console.log(dia);
+
+//Exercicio 1
+// Peça 2 numeros e retorne o maior
+
+var num1 = prompt("Digite 1 numero");
+var num2 = prompt("Digite o 2 numero");
+var num3 = prompt("Digite o 3 numero");
+var x;
+
+if (num1 > num2 && num1 > num3){
+    console.log(num1);
+} if (num2 > num2){
+    console.log(num2);
+}
