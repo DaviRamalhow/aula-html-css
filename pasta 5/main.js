1befaa0963eb536fb811a60a49ea9f06f9306a29
Davi Ramalho <daviramalho3011@gmail.com>
2022-06-07 19:20:57 -0300

    menor e maior

--- pasta 5/main.js
+++ pasta 5/main.js
@@ -21,13 +21,40 @@
 //Exercicio 1
 // Peça 2 numeros e retorne o maior
 
-var num1 = prompt("Digite 1 numero");
-var num2 = prompt("Digite o 2 numero");
-var num3 = prompt("Digite o 3 numero");
+var num1 = prompt("Digite o primeiro numero");
+var num2 = prompt("Digite o segundo numero");
+var num3 = prompt("Digite o terceiro numero");
 var x;
 
+
+//---------- Maior Numero ----------
 if (num1 > num2 && num1 > num3){
-    console.log(num1);
-} if (num2 > num2){
-    console.log(num2);
+    console.log("O maior numero é " + num1);
+
+} 
+
+if (num2 > num1&&num2>num3){
+    console.log("O maior numero é " + num2);
+
+} 
+
+if (num3 > num1&&num3 > num2){
+    console.log("O maior numero é " + num3);
+}
+
+
+
+//---------- Menor ----------
+if (num1 < num2 && num1 < num3){
+    console.log("O menor numero é " + num1);
+
+} 
+
+if (num2 < num1 && num2 < num3){
+    console.log("O menor numero é " + num2);
+
+} 
+
+if (num3 < num1 && num3 < num2){
+    console.log("O menor numero é " + num3);
 }
