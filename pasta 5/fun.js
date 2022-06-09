78996eade3440ca6fd0f14fc22b56d77f45134b5
Davi Ramalho <daviramalho3011@gmail.com>
2022-06-08 21:49:44 -0300

    New content [addEventListener]

--- pasta 5/fun.js
+++ pasta 5/fun.js
@@ -14,3 +14,38 @@
     function  clique (){
         alert("Oi Galera");
     }
+
+
+
+    function add (){
+
+        var input1 = document.getElementById("num1");
+        var input2 = document.getElementById("num2");
+        console.log(input1.value);
+        console.log(input2.value);
+
+        var soma = parseInt(input1.value)+ parseInt(input2.value);
+        console.log(soma);
+
+        var resultado = document.getElementById("result");
+        resultado.innerHTML = soma;
+
+    
+    }
+    
+
+
+    
+    function tratar (evento){
+
+        
+        if(evento.keyCode <=48 || evento.keyCode >= 57){
+        evento.preventDefault();
+        }
+        console.log(evento);
+        console.log("dentro de tratar")
+
+    }
+
+    var input1 = document.getElementById("num1");
+    input1.addEventListener("keydown",tratar);
